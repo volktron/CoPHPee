349a8237549e586b15f6f17f0f90e2457dae59bb
Henrik Volckmer <henrik.volckmer@gmail.com>
2023-07-25 17:57:26 -0400

    Working Controller test case

---
+++ tests/endpoints/IndexControllerTest.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+
+namespace tests\endpoints;
+
+use tests\AppTestCase;
+
+class IndexControllerTest extends AppTestCase
+{
+    public function testDefault() {
+        $result = $this->get('');
+
+        $this->assertIsArray($result);
+        $this->assertIsString($result['output']);
+
+        $this->assertEquals('hello', json_decode($result['output'])->message);
+    }
+}

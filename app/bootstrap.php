6e8c2f197e643bb0b92403f2a5d3e7f389df2304
Henrik Volckmer <henrik.volckmer@gmail.com>
2023-07-25 18:26:16 -0400

    Cleanup + `composer.lock`

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,10 +4,6 @@ use app\App;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// Initialize Session, DB, etc.
-
 // Routing
 $app = new App();
 $app->execute($_SERVER['REQUEST_METHOD'], $_GET['path']);
-
-// Fallback
